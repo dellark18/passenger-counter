abf71dec64354ddea7fd86ca2a206894bc74ef45
Dele <ajayibamidele60@gmail.com>
2023-08-06 14:01:29 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// let bonusPoints = 50
+// bonusPoints = bonusPoints + 50
+
+// console.log(bonusPoints)
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+function increment() {
+    console.log("The button was clicked")
+}
+// change the count-el in the HTML to reflect the new count
